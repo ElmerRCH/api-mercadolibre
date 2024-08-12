21489f3787c4f615f12e90413397d0e967830e8c
Elmer <renehiguera15@gmail.com>
2024-08-12 15:21:27 -0600

    cleaners

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ HEADERS = {
     "Authorization": f"Bearer {ACCESS_TOKEN}"
 }
 
+MARCA = "urrea"
+
 # Función para obtener el modelo del producto desde los atributos
 def get_model_from_attributes(attributes):
     for attribute in attributes:
@@ -47,7 +49,7 @@ def comparar_y_actualizar_precio(row):
         # Filtrar productos que contengan "urrea" en el nombre y coincidan en modelo
         productos_filtrados = [
             item for item in data["results"]
-            if "urrea" in item["title"].lower() and  get_model_from_attributes(item.get("attributes", [])) == modelo_mio
+            if MARCA in item["title"].lower() and  get_model_from_attributes(item.get("attributes", [])) == modelo_mio
         ]   
         
         # Comparar precios solo con productos filtrados
@@ -75,7 +77,7 @@ async def listar_productos(query: str = "all", limit: int = 260 ):
             
             "limit": 50,  # Número de resultados por página
             "offset": offset,  # Página de resultados
-            "q": "urrea",  # Palabra clave de búsqueda
+            "q": MARCA,  # Palabra clave de búsqueda
             "seller_id": "344549261",  # ID del vendedor
         }
 
@@ -140,17 +142,77 @@ async def listar_productos(query: str = "all", limit: int = 260 ):
     wb.save(f"{Paths.PATH_EXCEL.value}{nombre_archivo}")
 
     return item
- 
 
-@app.post("/get-excel")
-async def listar_productos(query: str, limit: int = 260):
-    return query
+paths = [
+    "data_excel/bellota/bellota.xlsx",
+    "data_excel/urrea/urrea.xlsx",
+    "data_excel/gamo/gamo.xlsx",
+    "data_excel/bosch/bosch.xlsx"
+]
+
+@app.get("/limpiar-repetidos-nombre")
+async def limpiar_repetidos():
+   
+    try:
+        for i in paths:
+            # Leer el archivo Excel
+            print(f"Procesando: {i}")
+            df = pd.read_excel(i)
+
+            # Limpiar espacios en blanco en los nombres de productos
+            df["PRODUCTO"] = df["PRODUCTO"].str.strip()
+
+            # Identificar y eliminar productos repetidos
+            def process_duplicates(group):
+                if len(group) > 1:
+                    # Si hay duplicados
+                    group = group.sort_values(by='CODIGO', ascending=False)
+                    if group['CODIGO'].notna().any():
+                        # Mantener el primero con código
+                        group = group.dropna(subset=['CODIGO'])
+                    return group.head(1)
+                return group
+
+            df = df.groupby('PRODUCTO').apply(process_duplicates).reset_index(drop=True)
+            
+            # Guardar el archivo limpio
+            df.to_excel(i, index=False)
+
+        return {"mensaje": "Productos repetidos eliminados", "archivo_guardado": i}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al procesar el archivo Excel: {str(e)}")
+
+
+@app.get("/limpiar-repetidos-codigo")
+async def limpiar_repetidos():
+   
+    try:
+        for i in paths:
+            # Leer el archivo Excel
+            print(f"Procesando: {i}")
+            df = pd.read_excel(i)
+
+            df["CODIGO"] = df["CODIGO"].str.strip()
+            df = df.drop_duplicates(subset=["CODIGO"], keep='first')
+
+            # Guardar el archivo limpio
+            df.to_excel(i, index=False)
+
+        return {"mensaje": "Productos repetidos eliminados", "archivo_guardado": i}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al procesar el archivo Excel: {str(e)}")
+
+@app.get("/get-excel")
+async def listar_productos( limit: int = 260):
+
     try:
         # Leer el archivo Excel de Mercado Libre
         df_ml = pd.read_excel("data_excel/general/mercadolibre.xlsx")
-
+        
         # Filtrar productos que contengan la palabra clave en su nombre
-        productos_filtrados = df_ml[df_ml['TITLE'].str.contains(query, case=False, na=False)]
+        productos_filtrados = df_ml[df_ml['TITLE'].str.contains(MARCA, case=False, na=False)]
 
         # Limitar el número de productos a 'limit'
         productos_filtrados = productos_filtrados.head(limit)
@@ -159,7 +221,7 @@ async def listar_productos(query: str, limit: int = 260):
         wb = openpyxl.Workbook()
         ws = wb.active
 
-        ws.title = query
+        ws.title = MARCA
 
         # Escribir los encabezados
         headers = ["CANT.", "CODIGO", "PRODUCTO", "VENTAS", "PRECIO", "P.COMP", "P.COSTO"]
@@ -178,8 +240,8 @@ async def listar_productos(query: str, limit: int = 260):
             ])
 
         # Guardar el archivo Excel
-        nombre_archivo = f"{query}.xlsx"
-        wb.save(f"data_excel/{query}/{query}.xlsx")
+        nombre_archivo = f"{MARCA}.xlsx"
+        wb.save(f"data_excel/{MARCA}/{MARCA}.xlsx")
 
         return {"mensaje": "Archivo Excel generado exitosamente", "ruta": nombre_archivo}
     
@@ -190,7 +252,9 @@ async def listar_productos(query: str, limit: int = 260):
 async def comparar_precios():   
     try:
 
-        EXCEL = f"{Paths.PATH_EXCEL.value}URREA/URREA.xlsx"
+        
+        EXCEL = f"{Paths.PATH_EXCEL.value}{MARCA}/{MARCA}.xlsx"
+        print(EXCEL)
         # Leer el archivo Excel
         df = pd.read_excel(EXCEL)
 
@@ -222,8 +286,6 @@ async def comparar_precios():
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error al procesar el archivo Excel: {str(e)}")
-
-
 
 @app.get("/actualizar-cantidad")
 async def actualizar_cantidad():
